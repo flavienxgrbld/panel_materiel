3fdf1fd3c5c1604c8d7b254a9ba9a54c23161ee4
flavienxgrbld <flavien.garibaldi@gmail.com>
2025-09-04 13:50:25 +0200

    update menu.py
    ajout du boutton parametre + creation fonction ouvrir_parametres

--- menu.py
+++ menu.py
@@ -21,10 +21,17 @@ def verif_requirements():
         valid_label.grid_remove()
         btn_interface1.grid(column=0, row=0, padx=10, pady=10)
         btn_interface2.grid(column=1, row=0, padx=10, pady=10)
+        btn_parametres.grid(column=1, row=1, padx=10, pady=10)
         
     except subprocess.CalledProcessError as e:
         messagebox.showerror("Erreur", f"Échec installation requirements:\n{e}")
         loading_label.grid_remove()
+
+
+def ouvrir_parametres():
+    messagebox.showinfo("Paramètres", "Ici, vous pouvez ajouter vos paramètres.")
+
+
 def gestion_materiel():
     try:
         subprocess.Popen(["python", "gestion-materiel.py"])
@@ -60,5 +67,8 @@ valid_label = ttk.Label(frame, text="✅ Chargement validé", foreground="green"
 
 btn_interface1 = ttk.Button(frame, text="Gestion De Materiel", command=gestion_materiel)
 btn_interface2 = ttk.Button(frame, text="Fiche Remise Materiel", command=fiche_remise_materiel)
+
+btn_parametres = ttk.Button(frame, text="Paramètres", command=ouvrir_parametres)
+
 
 oWindows.mainloop()
